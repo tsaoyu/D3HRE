741057d1d011323bff948ef2f845f27c2faa4d8e
tsaoyu <tsaoyu@tsaoyu.com>
2017-07-24 14:36:13 +0100

    add Mission class for future use

--- mission_utlities/route_manager.py
+++ mission_utlities/route_manager.py
@@ -1,4 +1,8 @@
+import hashlib
+from datetime import timedelta
+
 import numpy as np
+import pandas as pd
 from math import radians, cos, sin, asin, sqrt
 
 def opencpn_coordinates_processing(coordinates):
@@ -151,7 +155,6 @@ def hashFor(data):
     """
     hashId = hashlib.md5()
     hashId.update(repr(data).encode('utf-8'))
-
     return hashId.hexdigest()
 
 def get_position_df(start_time, route, speed):
@@ -163,5 +166,23 @@ def get_position_df(start_time, route, speed):
     return position_df
 
 
-class
+class Mission():
+
+    def __init__(self, start_time, route, speed):
+        self.start_time = start_time
+        self.route = route
+        self.speed = speed
+        self.df = get_position_df(self.start_time, self.route, self.speed)
+
+    def generate_more(self):
+        pass
+
+
+if __name__ == '__main__':
+
+    route1 = opencpn_coordinates_processing(
+        "50.1871,26.1087,0. 52.5835,26.431,0. 54.1412,26.1087,0. 55.9985,26.5919,0. 57.017,25.9472,0. 57.9157,24.8105,0. 60.3721,23.5535,0. 61.091,20.4993,0. 59.054,18.1817,0. 56.6575,16.4085,0. 52.5835,14.445,0. 49.4082,13.0484,0. 45.3941,11.937,0. 43.8364,12.23,0. 41.6197,15.1402,0. 39.8823,18.2955,0. 37.9651,21.7288,0. 36.1677,24.5928,0. 35.0294,26.431,0. 32.8726,29.2385,0. 30.7158,32.7302,0. 26.7616,33.8818,0. 20.7705,34.8216,0. 15.678,35.2631,0. 11.4841,37.3867,0. 6.99077,38.0029,0. 2.55731,37.6243,0. -1.27704,36.6211,0. -7.50785,35.7021,0. -10.9228,35.0672,0. -17.4532,34.8708,0. -24.8223,34.8708,0. -30.9333,34.4272,0. -38.1227,34.1797,0. -46.9297,33.483,0. -52.142,33.0321,0. -58.4927,32.4779,0. -64.4838,32.0727,0. -70.0556,31.8694,0. -75.3278,31.8694,0. -78.6829,31.4614,0. -81.3789,31.4103,0. ")
+
+    mission1 = Mission('2014-01-01', route1, 2)
+    print(hashFor(mission1)[0:7])
 
