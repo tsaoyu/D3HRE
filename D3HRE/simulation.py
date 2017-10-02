db8447e459246e60c438993a2aa1924ab8f8b3b8
Tony Yu Cao <tsaoyu@gmail.com>
2017-10-02 16:25:55 +0100

    new Sim class based on oop

--- D3HRE/simulation.py
+++ D3HRE/simulation.py
@@ -11,6 +11,8 @@ from opendap_download import route_based_download
 from D3HRE.core.dataframe_utility import full_day_cut
 from D3HRE.core.battery_models import min_max_model, soc_model_fixed_load
 from D3HRE.core.wind_turbine_model import  power_from_turbine, resistance_power
+from D3HRE.core.mission_utility import Mission
+
 
 @lru_cache(maxsize=32)
 def power_unit_area(start_time, route, speed, power_per_square=140,
@@ -108,6 +110,85 @@ def temporal_optimization(start_time, route, speed, solar_area, wind_area, use, 
             return  LPSP, SOC, energy_history, unmet_history, waste_history, use_history, power
     else:
         return  LPSP
+
+
+class Sim:
+
+    def __init__(self, mission):
+        self.mission = full_day_cut(mission.df)
+        self.resource_df = None
+        self.solar_power = 0
+        self.wind_power = 0
+        self.battery_energy = 0
+
+    def own_fun(self, inputs):
+        return inputs+1
+
+    @property
+    def get_resource_df(self):
+        self.resource_df = route_based_download.resource_df_download_and_process(self.mission)
+        return self.resource_df
+
+    def sim_wind(self, area, power_coefficient=0.3, cut_in_speed=2, rated_speed=15):
+        """
+        Simulation wind power generation considering the added resistance of wind due the wind turbine.
+
+        :param area: float m^2 wind turbine swept area
+        :param power_coefficient: float power coefficient of wind turbine
+        :param cut_in_speed: float m/s cut-in speed of wind turbine
+        :param rated_speed: float m/s cut-out speed of wind turbine
+        :return: Nothing returned but wind_power Pandas series was created in class
+        """
+
+
+        wind_df = self.get_resource_df
+        # Apply wind speed to ideal wind turbine model, get power production correction due to speed
+        wind_df['wind_power'] = wind_df.V2.apply(
+            lambda x: power_from_turbine(x, area, power_coefficient, cut_in_speed, rated_speed)) - \
+                                resistance_power(wind_df, area)
+        self.wind_power_raw = wind_df.V2.apply(
+            lambda x: power_from_turbine(x, area, power_coefficient, cut_in_speed, rated_speed))
+        self.wind_power = wind_df.wind_power
+        pass
+
+    def sim_solar(self, tilt, azim, tracking, capacity,
+                  technology='csi', system_loss=0.10, angles=None, dataFrame=False,
+                  **kwargs):
+        """
+        Simulate solar energy production based on various input of renewable energy system.
+
+        :param tilt: float degrees title angle of PV panel
+        :param azim: float degrees azim angle of PV panel
+        :param tracking: int 0 1 or 2 0 for no tracking, 1 for one axis, 2 for two axis
+        :param technology: optional str 'csi'
+        :param system_loss: float system lost of the system
+        :param angles: optional solar angle
+        :param dataFrame: optional return dataframe or not
+        :return: tuple of Pandas series solar power and wind power with datatime index
+        """
+        solar_df = self.get_resource_df
+        solar_df['global_horizontal'] = solar_df.SWGDN
+        solar_df['diffuse_fraction'] = brl_model.location_run(solar_df)
+        solar_df['solar_power'] = pv.run_plant_model_location(solar_df, tilt, azim,
+                                                              tracking, capacity, technology,
+                                                              system_loss, angles, dataFrame, **kwargs)
+        self.solar_power = solar_df.solar_power
+        pass
+
+    def sim_all(self, use, battery_capacity, shading_coef=0.05):
+        """
+        Simulation on both wind and PV system for hybrid renewable energy system.
+
+        :param use:float Load of the system
+        :param battery_capacity: float Wh total capacity of battery
+        :return: None but update the remianing energy in battery
+        """
+        power = (1 - shading_coef)*self.solar_power + self.wind_power
+        battery_energy = min_max_model(power, use, battery_capacity)
+        self.battery_energy = pd.Series(battery_energy, index=self.mission.index)
+        return self.battery_energy
+
+
 
 
 class Simulation:
