57acaf4e9f029062d1ecaaf1a0301a80f3fafa93
Yu Cao <tsaoyu@tsaoyu.com>
2018-05-25 15:36:30 +0100

    Test download now use test_env

--- tests/test_download.py
+++ tests/test_download.py
@@ -2,29 +2,13 @@
 # but it give programmer more confidence that it can make sure some functions works as expected.
 
 import pytest
-import numpy as np
-import os, sys
-from passwords import *
 
-sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir))
+from tests.test_env import *
+from D3HRE.core.weather_data_download import resource_df_download
 
 
-
-from D3HRE.core.mission_utility import get_mission
-from D3HRE.core.weather_data_download import download_URL, resource_df_download
-
-
-# Make sure that generated link matches the total length of original mission
-test_route =  np.array([[  10.69358 ,  -178.94713892], [  11.06430687, +176.90022735]])
-test_mission = get_mission('2014-01-01', test_route, 2)
-
-
-def test_duration():
-    start, end = download_URL(test_mission, debug=True)
-    assert end - start == test_mission.index[-1] - test_mission.index[0]
-    pass
 
 def test_resource_df_download():
-    resource_df_download(test_mission, username=USERNAME, password=PASSWORD, n=1)
+    resource_df_download(test_mission)
     pass
 
