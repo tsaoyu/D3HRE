64b8c0c3d41ba5d698bfa8e44b025c3602ee7564
Yu Cao <tsaoyu@tsaoyu.com>
2019-03-08 17:10:15 +0000

    Adjust the multiple objective function implementation

--- D3HRE/optimization.py
+++ D3HRE/optimization.py
@@ -33,9 +33,9 @@ class Mixed_objective_optimization_function:
             volume_factor = self.config['optimization']['constraints']['volume_factor']
             water_plane_coff = self.config['optimization']['constraints']['water_plane_coff']
             turbine_diameter_ratio = self.config['optimization']['constraints']['turbine_diameter_ratio']
-            max_solar_area = self.Task.vehicle.maximum_deck_area() * water_plane_coff
-            max_wind_area = (turbine_diameter_ratio * self.Task.vehicle.beam) ** 2 * np.pi / 4
-            max_battery_capacity = self.Task.vehicle.displacement * volume_factor * 1000 * SED
+            max_solar_area = self.Task.robot.surface_area * water_plane_coff
+            max_wind_area = (turbine_diameter_ratio * self.Task.robot.beam) ** 2 * np.pi / 4
+            max_battery_capacity = self.Task.robot.displacement * volume_factor * 1000 * SED
 
         except KeyError:
 
@@ -65,6 +65,16 @@ class Mixed_objective_optimization_function:
     def get_nobj(self):
         return 1
 
+class Multiple_objective_optimization_function(Mixed_objective_optimization_function):
+
+    def fitness(self, x):
+        weight = self.weight
+        capital_cost = x[0] * weight[0] + x[1] * weight[1] + x[2] * weight[2]
+        lpsp = self.reactive_sim.run(x[0], x[1], x[2])
+        return [capital_cost, lpsp]
+
+    def get_nobj(self):
+        return 2
 
 class Constraint_mixed_objective_optimisation(Mixed_objective_optimization_function):
     def __init__(self, Task, config={}):
@@ -172,6 +182,65 @@ class Constraint_mixed_objective_optimisation(Mixed_objective_optimization_funct
         resource = result_df.wind_power + result_df.solar_power
         with open(name, 'wb') as f:
             cloudpickle.dump([system, result_df, resource], f)
+
+class Constraint_multiple_objective_optimisation(Multiple_objective_optimization_function):
+    def __init__(self, Task, config={}, algorithm='nsga-2'):
+        """
+        Basic class for the constraint mixed objective optimisation problem. The optimisation
+        start by providing the Task object to the problem. It will formed as an optimisation
+        problem using the mixed objective optimisation function. When all the parameters are set,
+        call run() method to run the optimisation.
+
+        :param Task: task object (mission + vehicle )
+        :param config: configuration file if exist will be pass to mixed objective function
+        """
+        self.config = config
+        self.Task = Task
+        self.set_parameters()
+        self.algorithm_type = algorithm
+        if config != {}:
+            self.problem = pg.problem(
+                Multiple_objective_optimization_function(Task, self.config)
+            )
+        else:
+            self.problem = pg.problem(Multiple_objective_optimization_function(Task))
+
+        self.rea_sim = simulation.Reactive_simulation(self.Task, config=self.config)
+
+    def set_parameters(self):
+        try:
+            self.generation = self.config['optimization']['method']['pso']['generation']
+            self.pop_size = self.config['optimization']['method']['pso']['population']
+        except KeyError:
+            self.generation = 100
+            self.pop_size = 100
+
+    def run(self):
+        """
+        Run the optimisation process using PSO algorithm.
+        :param converge_info: optional run the optimisation with convergence information
+        :param converge_info: optional run the optimisation with population information
+        :return:
+        """
+        print("Start the optimisation process...")
+
+        if self.algorithm_type == 'nsga-2':
+            uda = pg.nsga2(gen=self.generation)
+        elif self.algorithm_type == 'moea-d':
+            uda = pg.moead(gen=self.generation)
+        elif self.algorithm_type == 'ihs':
+            uda = pg.ihs(gen=self.generation)
+
+        algo = pg.algorithm(uda)
+        pop = pg.population(self.problem, self.pop_size)
+        pop = algo.evolve(pop)
+        self.pop = pop
+        return pop
+
+    def plot_non_dominated_fronts(self):
+        return pg.plot_non_dominated_fronts(self.pop.get_f())
+
+
 
 
 if __name__ == '__main__':
